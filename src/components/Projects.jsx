6305bae265e5b18c15d1f8155350990be3350aa8
Al-Iskandari <jhoniimx@gmail.com>
2025-02-27 08:37:51 +0700

    update projects object

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,15 +10,15 @@ function Projects() {
   const prevItemCount = UsePrevious(itemCount);
 
   const projects = [
-    { id: 1, title: 'Project 1', description: 'Description of Project 1',link:'', screenshot: ''},
-    { id: 2, title: 'Project 2', description: 'Description of Project 2',link:'', screenshot: '' },
-    { id: 3, title: 'Project 3', description: 'Description of Project 3',link:'', screenshot: '' },
-    { id: 4, title: 'Project 4', description: 'Description of Project 4',link:'', screenshot: ''},
-    { id: 5, title: 'Project 5', description: 'Description of Project 5',link:'', screenshot: '' },
-    { id: 6, title: 'Project 6', description: 'Description of Project 6',link:'', screenshot: '' },
-    { id: 7, title: 'Project 7', description: 'Description of Project 7',link:'', screenshot: '' },
-    { id: 8, title: 'Project 8', description: 'Description of Project 8',link:'', screenshot: '' },
-    { id: 9, title: 'Project 9', description: 'Description of Project 9',link:'', screenshot: '' },
+    { id: 1, title: 'Blog Admin Backend-express-typescript', description: 'This project represents a RESTful API built with Node.js, TypeScript and Sequelize, an ORM for interacting with databases. The framework suggests organized development, with features such as authentication, file uploads, data validation, and database migrations.',link:'https://github.com/Al-Iskandari/blog-admin-backend-express-typescript', screenshot: 'express-typescript_1.png', sourceLogo:''},
+    { id: 2, title: 'Blog Admin React + Vite', description: 'Blog Admin is a web-based admin panel built using React. It includes various UI components, themes, and structured page layouts for easy customization.',link:'https://github.com/Al-Iskandari/blog-admin-frontend-react-vite', screenshot: 'Gemini_Generated_Image_r6ekf5r6ekf5r6ek-removebg-preview.png' },
+    { id: 3, title: 'Circle timer with CSS Animation', description: 'Simple circle timer using css animation keyframe to run circle element based on specified duration and roll all circle element along roll specified time.',link:'https://codepen.io/al-iskandari/pen/jENNXgq', screenshot: 'circle_timer_css_animation.png' },
+    { id: 4, title: 'Project 4', description: 'Description of Project 4',link:'', screenshot: 'Gemini_Generated_Image_r6ekf5r6ekf5r6ek-removebg-preview.png'},
+    { id: 5, title: 'Project 5', description: 'Description of Project 5',link:'', screenshot: 'Gemini_Generated_Image_r6ekf5r6ekf5r6ek-removebg-preview.png' },
+    { id: 6, title: 'Project 6', description: 'Description of Project 6',link:'', screenshot: 'Gemini_Generated_Image_r6ekf5r6ekf5r6ek-removebg-preview.png' },
+    { id: 7, title: 'Project 7', description: 'Description of Project 7',link:'', screenshot: 'Gemini_Generated_Image_r6ekf5r6ekf5r6ek-removebg-preview.png' },
+    { id: 8, title: 'Project 8', description: 'Description of Project 8',link:'', screenshot: 'Gemini_Generated_Image_r6ekf5r6ekf5r6ek-removebg-preview.png' },
+    { id: 9, title: 'Project 9', description: 'Description of Project 9',link:'', screenshot: 'Gemini_Generated_Image_r6ekf5r6ekf5r6ek-removebg-preview.png' },
   ];
   const totalCount = projects.length;
 
@@ -44,7 +44,7 @@ function Projects() {
           <div key={index} className={`${itemCount} p-4 md:w-1/3 ${(index > (itemCount-4))?"show-more":""}`}>
             <div className="flex rounded-lg h-full bg-gray-200 p-8 flex-col">
               <div className="lg:max-w-lg lg:w-full">
-                <img className="object-cover object-center rounded" alt="hero" src="./images/Gemini_Generated_Image_r6ekf5r6ekf5r6ek-removebg-preview.png" />
+                <img className="object-cover object-center rounded" alt="hero" src={`./images/${project.screenshot}`} />
               </div>
               <div className="flex items-center mb-3">
                 <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
@@ -54,7 +54,7 @@ function Projects() {
               </div>
               <div className="flex-grow">
                 <p className="leading-relaxed text-base">{project.description}</p>
-                <a href="" className="mt-3 text-indigo-500 inline-flex items-center">Learn More
+                <a href={project.link} className="mt-3 text-indigo-500 inline-flex items-center">Learn More
                   <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="w-4 h-4 ml-2" viewBox="0 0 24 24">
                     <path d="M5 12h14M12 5l7 7-7 7" />
                   </svg>
