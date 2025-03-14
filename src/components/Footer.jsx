66276989644dec72db44fd021418558c0227f7ed
Al-Iskandari <jhoniimx@gmail.com>
2025-03-15 06:58:49 +0700

    set footer position to absolute

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import 'react'
 
 function Footer() {
   return (
-    <footer className="left-0 right-0 mb-0">
+    <footer className="absolute left-0 right-0 mb-0">
       <div className="container mx-auto py-5 bg-white shadow sm:shadow-none border-t-4 border-blue-500">
         <p className="flex flex-row items-center justify-center text-center text-sm text-slate-700">©Copyright {new Date().getFullYear()} | <img className="w-10 h-10 text-white" width="100px" height="100px" src="./images/favicon.png" /></p>
       </div>
