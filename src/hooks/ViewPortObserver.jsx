541632ab56dfff1e3ff2a2c7895c3fce98123612
Al-Iskandari <jhoniimx@gmail.com>
2025-10-25 08:33:12 +0700

    update viewPortObserver logic

--- src/hooks/ViewPortObserver.jsx
+++ src/hooks/ViewPortObserver.jsx
@@ -1,14 +1,31 @@
 import { useEffect, useState } from "react";
 
 export function ViewPortObserver(ref) {
-    const [isIntersecting, setIntersecting] = useState(false);
+  const [isIntersecting, setIntersecting] = useState(false);
+
+    
 
   useEffect(() => {
+    if (!ref.current) {
+      return;
+    }
+    // Get the initial viewport height on component mount
+    const initialViewportHeight = window.innerHeight;
+    
+    // Calculate the dynamic threshold based on the ratio
+    const dynamicThreshold = initialViewportHeight * 0.5;
+
+    const observerOptions = {
+      root: null, // use the document's viewport
+      rootMargin: `-${dynamicThreshold}px 0px`, // Use negative margin for a dynamic offset
+      threshold: 0, // No threshold is needed because we use rootMargin
+    };
+
     const observer = new IntersectionObserver(([entry]) => {
         setIntersecting(entry.isIntersecting)
 
-    },{
-      rootMargin:"-60%"} //Adjust this value
+    },
+      observerOptions
     );
 
     observer.observe(ref.current);
